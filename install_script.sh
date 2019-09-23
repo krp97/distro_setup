6710df99bdc479489e102bdca0ce1ed4419a3b9e
krp97 <k.plata576@gmail.com>
2019-09-23 22:10:10 +0200

    Added arguments & pacman checks

--- install_script.sh
+++ install_script.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+# Installs stow and places symlinks to all dotfiles in home directory.
+
 # Colorful output in terminals that support colors.
 # Ripped straight from SO.
+
+no_confirm=false
+skip_pacman_update=false
+dotfile_path='~/'
+dry_run=false
+force_overwrite=false
 
 if [[ -t 1 ]]; then
     ncolors=$(tput colors)
@@ -19,45 +27,83 @@ if [[ -t 1 ]]; then
     fi
 fi
 
-# Switches:
-#   --noconfirm  -> headless install
-#   --skipupdate -> skip pacman -Syu
-#   --dotfiles   -> specify folder for dotfiles symlinks (Default home)
-#   --dry        -> echo only run
-#   --force      -> force stow to overwrite existing symlinks
+usage() {
+    printf "Usage:\n"
+    printf "\t%2s \t%s\n" "${bold}${white}-n${normal}" "No confirmation will be asked from user during script execution."
+    printf "\t%2s \t%s\n" " " "Do note that pacman also won't ask for sudo password,"
+    printf "\t%2s \t%s\n\n" " " "so you have to run the script with elevated privileges."
 
-stow_install() {
-    superuser=$1
-    skip_update=$2
+    printf "\t%2s \t%s\n" "${bold}${white}-s${normal}" "Skips pacman -Syu."
+    printf "\t%2s \t%s\n" "${bold}${white}-d${normal}" "Specify the path to symlinks directory (Default: home)."
+    printf "\t%2s \t%s\n" "${bold}${white}-e${normal}" "Do not make any changes (echo only run)."
+    printf "\t%2s \t%s\n" "${bold}${white}-f${normal}" "Force overwriting symlinks in the destination path."
+}
 
-    echo "${bold}${green} ---> Updating pacman mirrors & packages${normal}"
-
-    if [[ $skip_update == "" ]]; then
-        $superuser pacman -Syu --noconfirm
-    fi
-
+exit_on_error() {
+    error_str=$1
     if [[ $(echo $?) -ne 0 ]]; then
-        echo "${bold}${red} ---> [Error]${normal} Pacman failed to update."
-        exit 1
-    fi
-
-    echo "${bold}${green} ---> Installing stow${normal}"
-
-    $superuser pacman -S --noconfirm stow
-    if [[ $(echo $?) -ne 0 ]]; then
-        echo "${bold}${red} ---> [Error]${normal} Pacman failed to install stow."
+        echo $error_str
         exit 1
     fi
 }
 
+stow_install() {
+    superuser=$1
+    args=$2
+
+    if $skip_pacman_update; then
+        echo "${bold}${yellow} ---# Skipping pacman update${normal}"
+    else
+        echo "${bold}${green} ---> Updating pacman mirrors & packages${normal}"
+        $superuser pacman -Syu $args --needed
+        exit_on_error "${bold}${red} ---> [Error]${normal} Pacman failed to update."
+    fi
+
+    pacman -Qi stow >/dev/null
+    if [[ $(echo $?) -eq 0 ]]; then
+        echo "${bold}${yellow} ---# Skipping stow install (package already present)${normal}"
+    else
+        echo "${bold}${green} ---> Installing stow${normal}"
+        $superuser pacman -S $args stow --needed
+        exit_on_error "${bold}${red} ---> [Error]${normal} Pacman failed to install stow."
+    fi
+}
+
+while getopts ":nsd:efh" opt; do
+    case ${opt} in
+    n) no_confirm=true ;;
+    s)
+        skip_pacman_update=true
+        pacman_skip="--noconfirm"
+        ;;
+    d) path=$OPTARG ;;
+    e)
+        dry_run=true
+        stow_params=$stow_params"--simulate"
+        ;;
+    f) stow_params=$stow_params"--override=." ;;
+    h)
+        usage
+        exit 0
+        ;;
+    *)
+        echo "${bold}${red} ---> [Error] Invalid argument $OPTARG"
+        usage
+        exit 1
+        ;;
+    esac
+done
+shift $((OPTIND - 1))
+
 if [ "$EUID" -ne 0 ]; then
-    stow_install "sudo"
+    stow_install "sudo" $pacman_skip
 else
-    stow_install
+    stow_install $pacman_skip
 fi
 
 echo "${bold}${green} ---> Creating symlinks in home to ~/.dotfiles/${normal}"
 mkdir -p ~/.dotfiles
-cp -r ./dotfiles/ ~/.dotfiles
+cp -a ./dotfiles/. ~/.dotfiles/
 cd ~/.dotfiles
-stow .
+
+stow . -t ~/ $stow_params
