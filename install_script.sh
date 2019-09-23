d95ac026d2ddca29f4c6a5e17c680620d87dddb6
krp97 <k.plata576@gmail.com>
2019-09-23 19:41:30 +0200

    Moved dotfiles and added basic stow command

--- install_script.sh
+++ install_script.sh
@@ -1,1 +1,21 @@
 #!/bin/bash
+
+# Switches:
+#   --noconfirm -> headless install
+#   --reinstall -> force reinstall of existing packages
+#   --dotfiles  -> specify folder for dotfiles symlinks (Default home)
+#   --dry-run   -> echo only run
+
+# Add a possibility to easily exclude packages(files with lists or smth)
+# Add a possibility to pass params to pacman
+
+# Main repo packages list:
+# stow - must have -> creates symlinks for all dotfiles in the home dir
+# git, git-lfs, python, python-pip, cmake, cppcheck, clang, texlive-most,
+# firefox, vim,
+
+# User repo packages list:
+# vscode, fzf, polybar, spotify
+pacman -S --noconfirm stow
+stow ./dotfiles
+ls ~/
