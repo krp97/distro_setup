cfa762ae27bb853754ea45e8a0fe4ff47c7f0b75
krp97 <k.plata576@gmail.com>
2019-09-23 20:05:47 +0200

    Added color output

--- install_script.sh
+++ install_script.sh
@@ -4,7 +4,7 @@
 #   --noconfirm -> headless install
 #   --reinstall -> force reinstall of existing packages
 #   --dotfiles  -> specify folder for dotfiles symlinks (Default home)
-#   --dry-run   -> echo only run
+#   --dry       -> echo only run
 
 # Add a possibility to easily exclude packages(files with lists or smth)
 # Add a possibility to pass params to pacman
@@ -16,10 +16,34 @@
 
 # User repo packages list:
 # vscode, fzf, polybar, spotify
+
+# Ripped straight from SO
+if test -t 1; then
+
+    ncolors=$(tput colors)
+
+    if test -n "$ncolors" && test $ncolors -ge 8; then
+        bold="$(tput bold)"
+        normal="$(tput sgr0)"
+        black="$(tput setaf 0)"
+        red="$(tput setaf 1)"
+        green="$(tput setaf 2)"
+        yellow="$(tput setaf 3)"
+        blue="$(tput setaf 4)"
+        magenta="$(tput setaf 5)"
+        cyan="$(tput setaf 6)"
+        white="$(tput setaf 7)"
+    fi
+fi
+
+echo "${bold}${green} Updating pacman mirrors & packages${normal}"
 pacman -Syu --noconfirm
+echo "${bold}${green} Installing stow${normal}"
 pacman -S --noconfirm stow
+
+echo "${bold}${green} Creating symlinks in home to ~/.dotfiles/${normal}"
 mkdir -p ~/.dotfiles
 cp -r ./dotfiles/ ~/.dotfiles
 cd ~/.dotfiles
 stow .
-ls ~/
+ls -a ~/
