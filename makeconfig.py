9c91dd2b1c62a6f71bc37185c6a4d90f46d83a10
krp97 <k.plata576@gmail.com>
2020-08-26 21:28:30 +0200

    Updated script for linking dotfiles

--- makeconfig.py
+++ makeconfig.py
@@ -3,17 +3,27 @@
 import os
 import shutil
 
-FORCE_LINK = True
-
 repo_config_dir = os.path.join(os.path.dirname(__file__), "config")
-sys_config_dir = os.path.join(os.environ["HOME"], ".config")
+repo_dotfiles_dir = os.path.join(os.path.dirname(__file__), "dotfiles")
+
+sys_config_dir = os.path.join(os.environ['HOME'], ".config")
 os.makedirs(sys_config_dir, exist_ok=True)
 
+print(f"Creating symlinks for config/ in {sys_config_dir}")
 for app_dir in os.listdir(repo_config_dir):
     source = os.path.realpath(os.path.join(repo_config_dir, app_dir))
     target = os.path.join(sys_config_dir, app_dir)
     try:
         os.symlink(source, target)
     except OSError:
-        print(f"Skipping {target}, file exists")
-    
+        print(f"\t --> Skipping {target} -> {source}, file exists")
+
+
+print(f"\nCreating symlinks for dotfiles/ in {os.environ['HOME']}")
+for dotfile in os.listdir(repo_dotfiles_dir):
+    source = os.path.realpath(os.path.join(repo_dotfiles_dir, dotfile))
+    target = os.path.join(os.environ['HOME'], dotfile)
+    try:
+        os.symlink(source, target)
+    except OSError:
+        print(f"\t --> Skipping {target} -> {source}, file exists")
